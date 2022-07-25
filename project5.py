e57dd5f907c630797bbc2eeaf7727605df149d15
waiee <waiee_z@yahoo.com>
2022-07-25 21:57:49 +0800

    update text and markdown

--- project5.py
+++ project5.py
@@ -6,3 +6,9 @@ st.title("Testing Streamlit")
 #Header/subHeader
 st.header("This is header")
 st.subheader("This is a subheader")
+
+#Text
+st.text("Hello world")
+
+#Markdown
+st.markdown("### This is a markdown")
