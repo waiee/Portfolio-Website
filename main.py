2139bb414683f11208f89de325f9091581d64f83
waiee <waiee_z@yahoo.com>
2022-07-30 15:50:54 +0800

    section page

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+#### HORIZONTAL MENU#####
+# selected = option_menu(
+#         menu_title=None,
+#         options=["Home", "Projects", "Contact"],
+#         icons=["house", "book", "telephone"],
+#         default_index=0, #set homepage
+#         orientation="horizontal",
+#         styles={
+#             #will add later
+#         }
+#     )
+
 #Hide logo
 hide_st_style = """
             <style>
@@ -20,21 +32,17 @@ with st.sidebar:
         default_index=0, #set homepage
     )
 
-#### HORIZONTAL MENU#####
-# selected = option_menu(
-#         menu_title=None,
-#         options=["Home", "Projects", "Contact"],
-#         icons=["house", "book", "telephone"],
-#         default_index=0, #set homepage
-#         orientation="horizontal",
-#         styles={
-#             #will add later
-#         }
-#     )
-
+#HomePage
 if selected == "Home":
     st.title(f"{selected}")
+    from PIL import Image
+    img = Image.open("IMG_4825.jpeg")
+    st.image(img, width=300, caption="")
+
+#Projects
 if selected == "Projects":
-    st.title(f"{selected}")
+    st.title(f"{selected}")\
+
+#Contact
 if selected == "Contact":
     st.title(f"{selected}")
