15394a6687bdca2bc8c3e0a7865948ad05999056
waiee <waiee_z@yahoo.com>
2022-08-02 21:11:30 +0800

    change email address

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ if selected == "Contact":
 
         #Documentation
         contact_form = """
-        <form action="https://formsubmit.co/your@email.com" method="POST">
+        <form action="https://formsubmit.co/waiee_z@yahoo.com" method="POST">
             <input type="hidden" name="_captcha" value="false">
             <input type="text" name="name" placeholder="Your name" required>
             <input type="email" name="email" placeholder="Your Email" required>
