8ae30833ed1dc814238eb639b62880f7854f34f8
waiee <waiee_z@yahoo.com>
2022-08-02 20:45:30 +0800

    updating file, image selection

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from streamlit_option_menu import option_menu
 from PIL import Image
 
 st.set_page_config(page_title="waieezainol.com", page_icon=":computer:", layout="wide")
+dp_image = Image.open("IMG_4825.jpeg")
 
 #### HORIZONTAL MENU#####
 # selected = option_menu(
@@ -70,8 +71,7 @@ if selected == "Projects":
         image_column, text_column = st.columns((1,2))'
         with image_column:
             #insert images
-                img = Image.open("IMG_4825.jpeg")
-                st.image(img, width=500, caption="")
+                st.image(dp_image, width=500, caption="")
         with text_column:
             st.subheader("Testis Testis")
             st.write(
