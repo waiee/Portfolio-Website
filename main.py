2b10a28162d72ec0d6a733c608a8725c60082177
waiee <waiee_z@yahoo.com>
2022-08-02 21:07:11 +0800

    conatct column

--- main.py
+++ main.py
@@ -103,3 +103,8 @@ if selected == "Contact":
             <button type="submit">Send</button>
         </form> 
      """
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
