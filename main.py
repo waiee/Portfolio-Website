4b2063c099075b39f1894a70eeb37ecc2d5e0c25
waiee <waiee_z@yahoo.com>
2022-08-03 22:16:51 +0800

    getting local file

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}<style>", unsafe_allow_html=True)
 
-local_css("style.css")
+local_css("style/style.css")
 
 #--- LOAD ASSET ---
 lottie_file = load_lottie("https://assets7.lottiefiles.com/packages/lf20_dlw10cqe.json")
@@ -59,7 +59,7 @@ if selected == "Home":
             st.write("I am passionate in Data Science, Machine Learning, and Artificial Intelligence.")
             st.write("[Learn More >](https://github.com/waiee)")
         with right_column:
-            st_lottie(lottie_file, height=400 ,key="coding")
+            st_lottie(lottie_file,height=500 ,key="coding")
 
 ### WHAT I DO ###
     with st.container():
@@ -98,9 +98,9 @@ if selected == "Projects":
 
 # --- CONTACT ---
 if selected == "Contact":
-    st.title(f"{selected}")
+    # st.title(f"{selected}")
     with st.container():
-        st.write("---")
+        # st.write("---")
         st.header("Get In Touch With Me!")
         st.write("##")
 
