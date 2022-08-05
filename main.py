ef3bb5ae59f9a9f198618f9a0d804105e420120f
waiee <waiee_z@yahoo.com>
2022-08-05 15:49:02 +0800

    navbar

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/b
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #00000000;">
-  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Waiee Zainol</a>
+  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Waiee Zainol</a> 
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link disabled" href="">Home <span class="sr-only"></span></a>
+        <a class="nav-link" href="">Home <span class="sr-only"></span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="https://youtube.com/dataprofessor" target="_blank">YouTube</a>
